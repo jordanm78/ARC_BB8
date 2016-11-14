e701f10ca60f9cdb495efbf095a41d6d3acf4d21
unknown <w1573634@arc-ted-meyerj.arc.ad.losrios.edu>
2016-11-14 13:17:35 -0800

    Fixed the parsing routine so that it separates a command into a first part and a second part.  Removed print lines that were used for testing.  Removed delays that were used for testing.

--- bluetooth_control/bluetooth_control.ino
+++ bluetooth_control/bluetooth_control.ino
@@ -25,12 +25,20 @@ void loop()
 {
 //command = readBluetoothData();
 command = readSerialData();
-Serial.println(command);
-//Serial.println("Hello World!");
+if(command!="")
+{
+  Serial.println(command);
+}
 parsedString = getValue(command, ':', 0);
-//parsedString = "Hello World!";
-Serial.println(parsedString);
-delay(1000);
+if(parsedString !=0)
+{
+  Serial.println(parsedString);
+}
+parsedString = getValue(command, ':', 1);
+if(parsedString !=0)
+{
+  Serial.println(parsedString);
+}
  
  
   
@@ -50,18 +58,15 @@ if(BlueToothSerial.available())
         BluetoothCharacter = char(BlueToothSerial.read());
         if(BluetoothCharacter == 10) //ascii 10 is a line feed character command from phone must have a return
         {
-         //Serial.println("Command done!");
+
   
         }
         else
         {
         command += BluetoothCharacter;
-        //Serial.print(BluetoothCharacter);
         }
       } 
     }
- Serial.println(command);
- delay(1000);
  return command;    
 }
 
@@ -78,19 +83,18 @@ String readSerialData()
         Character = char(Serial.read());
         if(Character == 10) //ascii 10 is a line feed character command from phone must have a return
         {
-          //Serial.println("Command done!");
   
         }
         else
         {
         serialCommand += Character;
-        Serial.print(Character);
+        delay(1); //Delay 1 millisecond to allow more serial data to arrive.
         }
       } 
     }
   else
 {
-//Serial.println("No Serial Data to Read");
+
 }
 return serialCommand;
 }
@@ -98,21 +102,15 @@ return serialCommand;
 
 String getValue(String data, char separator, int index)
 {
-  Serial.println("I am looking through your string");
-  Serial.print("Your string is ");
-  Serial.println(data);
-  Serial.print("I am looking for ");
-  Serial.println(separator);
+
   int found = 0;
   int strIndex[] = {0, -1};
   int maxIndex = data.length()-1;
   for(int i=0; i<=maxIndex && found<=index; i++){
-  Serial.print(data.charAt(i));
   if(data.charAt(i)==separator || i==maxIndex){
   found++;
   strIndex[0] = strIndex[1]+1;
   strIndex[1] = (i == maxIndex) ? i+1 : i;
-  Serial.println("I found it!");
   }
  }
   return found>index ? data.substring(strIndex[0], strIndex[1]) : "";
